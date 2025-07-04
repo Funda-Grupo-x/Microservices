1d0b1257fbce08885b45204612f76160c3f16358
Sergio André Gómez Vallejos <102562782+CB-Sergio-AGV@users.noreply.github.com>
2025-07-03 19:39:46 -0500

    Delete iam-service directory

--- iam-service/src/main/java/pe/edu/upc/managewise/backend/iam/domain/exceptions/InvalidPasswordException.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.managewise.backend.iam.domain.exceptions;
-
-public class InvalidPasswordException extends RuntimeException {
-    public InvalidPasswordException() {
-        super("Invalid password");
-    }
-}
--- iam-service/src/main/java/pe/edu/upc/managewise/backend/iam/domain/services/RoleCommandService.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.managewise.backend.iam.domain.services;
-
-import com.agrotech.iamservice.iam.domain.model.commands.SeedRolesCommand;
-
-public interface RoleCommandService {
-    void handle(SeedRolesCommand command);
-}
